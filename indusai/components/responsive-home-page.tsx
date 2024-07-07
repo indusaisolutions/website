4386ef3895888fe988ec8ac5cb5416cc82eee22d
indusaisolutions <indusaisolutions@gmail.com>
2024-07-07 16:05:45 +0530

    changes

--- indusai/components/responsive-home-page.tsx
+++ indusai/components/responsive-home-page.tsx
@@ -144,8 +144,8 @@ export function ResponsiveHomePage() {
     </div>
   )
 }
+function MenuIcon(props: any) {
 
-function MenuIcon(props) {
   return (
     <svg
       {...props}
@@ -162,26 +162,6 @@ function MenuIcon(props) {
       <line x1="4" x2="20" y1="12" y2="12" />
       <line x1="4" x2="20" y1="6" y2="6" />
       <line x1="4" x2="20" y1="18" y2="18" />
-    </svg>
-  )
-}
-
-
-function MountainIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
     </svg>
   )
 }
